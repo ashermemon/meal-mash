6f394a674cf81faa8b19ca5bded957e2034ccbbe
ND0322 <nathandai03222@gmail.com>
2025-10-25 11:32:05 -0700

    Added login and signup placeholder functionality

--- server/models.py
+++ server/models.py
@@ -9,8 +9,5 @@ class User(db.Model):
     email = db.Column(db.String, unique = True)
     password = db.Column(db.String(30))
 
-    last_scraped_page = db.Column(db.Integer, default=1)
-    last_scraped_index = db.Column(db.Integer, default=0)
 
-    saved_posts = db.relationship('SavedPost', back_populates='user', cascade='all, delete-orphan')
 
