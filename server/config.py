fa1603b2814909774bf0dcadf3d684ebed12b8ab
ND0322 <nathandai03222@gmail.com>
2025-11-24 22:10:47 -0800

    Changes to config

--- server/config.py
+++ server/config.py
@@ -3,17 +3,27 @@ import os
 import redis
 
 load_dotenv()
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
     JWT_SECRET_KEY = SECRET_KEY
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
+
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
-    broker_url='redis://localhost:6379/0'
-    result_backend='redis://localhost:6379/0'
+
+    if("SESSION_REDIS" in os.environ):
+        SESSION_REDIS = redis.from_url(os.environ['SESSION_REDIS'])
+    else:
+        SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    
 
