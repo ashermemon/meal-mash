4fe2a63260e279e166d1a1e833ef775c68f44972
asher.memon27 <asher.memon27@stgeorges.bc.ca>
2026-09-04 11:26:28 -0700

    build settings (beta)

--- client/app.config.js
+++ client/app.config.js
@@ -14,6 +14,7 @@ export default {
       supportsTablet: true,
       infoPlist: {
         CFBundleDisplayName: "MealMash",
+        ITSAppUsesNonExemptEncryption: false,
       },
       bundleIdentifier: "com.sdsecureapp.neverleftovernew",
       icon: {
